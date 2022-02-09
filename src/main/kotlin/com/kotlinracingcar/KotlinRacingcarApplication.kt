c7b60cacf7b8f76e1df54a4c9e4d098a6512bc62
xrabcde <xrabcde@gmail.com>
2022-02-09 22:53:33 +0900

    feat: 자동차경주게임 환경세팅

---
+++ src/main/kotlin/com/kotlinracingcar/KotlinRacingcarApplication.kt
@@ -0,0 +1,11 @@
+package com.kotlinracingcar
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinRacingcarApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinRacingcarApplication>(*args)
+}
---
+++ src/test/kotlin/com/kotlinracingcar/KotlinRacingcarApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kotlinracingcar
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinRacingcarApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
